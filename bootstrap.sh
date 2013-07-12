6d42f3221ea41ca1532fb259e36a71429701546c
karlosmid <karlo.smid@gmail.com>
2013-07-12 14:30:42 +0200

    apache2 can see bopsky codebase

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 rm -rf /var/www
 ln -fs /vagrant/bopsky /var/www
+sudo touch /etc/httpd/conf.d/default-documentroot
+sudo chmod o+w  /etc/httpd/conf.d/default-documentroot
+sudo echo 'DocumentRoot /var/www' > /etc/httpd/conf.d/default-documentroot
 sudo service iptables stop
